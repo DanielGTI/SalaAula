9913a4f13d8bf930091ec275a92e407e3726a645
Daniel <daniel@gti.com>
2018-05-22 19:29:33 -0300

    Polimorfismo + Heranca

--- src/salaaula/Aluno.java
+++ src/salaaula/Aluno.java
@@ -1,20 +1,40 @@
-
 package salaaula;
 
-
 public class Aluno extends Pessoa {
-    
-        public String RA;
-    
-    	public Aluno(String nome, String cpf, String data) {
-		super(nome, cpf, data);
-	}
-	
-        @Override
-        public double tirarCopias(int qtd) {
-		return (0.08 * (double) qtd);
-	}
 
-       
+    public String RA;
+
+    public Aluno(String nome, String cpf, String data) {
+        super(nome, cpf, data);
+    }
+
+    @Override
+    public double tirarCopias(int qtd) {
+        return (0.08 * (double) qtd);
+    }
+
+    public double tirarCopias(int qtd, boolean frente_verso) {
+        if (frente_verso) {
+            return (0.07 * (double) qtd);
+        } else {
+            return (0.08 * (double) qtd);
+        }
+    }
+
+   
+    public double tirarCopias(int qtd, boolean frente_verso, String capa) {
+        if (frente_verso) {
+            return (0.07 * (double) qtd) + 10.0;
+        } else {
+            return (0.09 * (double) qtd) + 10.0;
+        }
+    }
+
+    /* POLIMORFISMO
         
+            SOBREPOSIÇÃO    (Mesma assinatura e classes diferentes)
+        
+            SOBRECARGA      (Assinatura diferente e mesma classe)
+        
+     */
 }
--- src/salaaula/SalaAula.java
+++ src/salaaula/SalaAula.java
@@ -8,17 +8,20 @@ public class SalaAula {
         System.out.println("\nNome: " + a.nome);
         System.out.println("CPF: " + a.cpf);
         System.out.println("Data de nascimento: " + a.data_nascimento);
-    
+
         System.out.println("Total de cópias = R$ " + a.tirarCopias(100));
-    
+
         System.out.println("----------------------");
         Professor p = new Professor("Neymar", "123.654.987-11", "02/06/1950");
         System.out.println("\nProfessor: " + p.nome);
         System.out.println("Total de cópias = R$ " + p.tirarCopias(100));
-    
-        
-        
-        
+
+        System.out.println("\n----------------------");
+        System.out.println("Copia para alunos (100)\n");
+        System.out.println("Cópias (Normal) = R$ "          + a.tirarCopias(100));
+        System.out.println("Cópias (Frente Verso) = R$ "    + a.tirarCopias(100, true));
+        System.out.println("Cópias (Capadura) = R$ "        + a.tirarCopias(100, true, "Daniel"));
+
     }
 
 }
